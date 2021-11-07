6116140da8f2ec1a4b80a03a525d5d0f2c606906
alexandre-andre <andregpf89@gmail.com>
2021-11-07 17:11:17 -0300

    insere exercicio 6

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicios/ex-1.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicios/ex-1.js
@@ -111,11 +111,19 @@ if(ang3 < 180 || ang3 < 0){
 /*
 6. Escreva um programa que receba o nome de uma peça de xadrez e retorne os movimentos que ela faz.
 - Como desafio, faça o programa funcionar tanto se receber o nome de uma peça com letras maiúsculas quanto com letras minúsculas, sem aumentar a quantidade de condicionais.
-- Como dica, você pode pesquisar uma função que faz uma string ficar com todas as letras minúsculas (lower case) .
+- Como dica, você pode pesquisar uma função que faz uma string ficar com todas as letras minúsculas (lower case).
 - Se a peça passada for inválida, o programa deve retornar uma mensagem de erro.
 - Exemplo: bishop (bispo) -> diagonals (diagonais)
 */
 console.log("Exercicio 6 - xadrez")
+let str = 'B!sPo'
+let string = str.toLocaleLowerCase();
+
+if(string == 'bispo'){
+    console.log('Movimento em diagonal até o limite de casas disponíveis')
+}else{
+    console.error(string + ' NÃO é uma peça válida')
+}
 
 
 
