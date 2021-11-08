c010c8d0846b8706377d0f341a6aec2d8b2e08d4
alexandre-andre <85708478+alexandre-andre@users.noreply.github.com>
2021-11-08 14:20:42 -0300

    Update fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicios/ex-1.js
    Co-authored-by: Rafael Bernard Araújo <rafael.araujo@initd.info>

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicios/ex-1.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicios/ex-1.js
@@ -41,7 +41,7 @@ console.log("Exercicio 2 - Maior entre 2")
 let num1 = 89
 let num2 = 93
 
-if(num1 > num2){
+if (num1 > num2) {
     console.log(`${num1} é maior que ${num2}`)
 }else if(num1 < num2){
     console.log(`${num1} é menor que ${num2}`)
